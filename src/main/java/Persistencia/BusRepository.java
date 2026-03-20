4b16df1a1ab9dd005c7f4c7e856265ec55e02a60
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-19 19:09:29 -0500

    feat: : creación repositorio busRepository (subclase)

---
+++ src/main/java/Persistencia/BusRepository.java
@@ -0,0 +1,45 @@
+package main.VehiculoService;
+import java.util.List;
+import Modelos.Bus;
+
+
+public class BusRepository {
+    private BusRepository repo = new BusRepository();
+
+    public void guardar(Bus v) throws Exception {
+        if (repo.buscarPorPlaca(v.getPlaca()) != null) {
+            throw new Exception("Ya existe un Bus con la placa: " + v.getPlaca());
+        }
+        repo.guardar(v);
+    }
+
+    public List<Bus> listar() throws Exception {
+        return repo.listar();
+    }
+
+    public Bus buscarPorPlaca(String placa) throws Exception {
+        return repo.buscarPorPlaca(placa);
+    }
+
+    public void actualizar(Bus v) throws Exception {
+        if (repo.buscarPorPlaca(v.getPlaca()) == null) {
+            throw new Exception("No existe un Bus con la placa: " + v.getPlaca());
+        }
+        repo.actualizar(v);
+    }
+
+    public void eliminar(String placa) throws Exception {
+        if (repo.buscarPorPlaca(placa) == null) {
+            throw new Exception("No existe un Bus con la placa: " + placa);
+        }
+        repo.eliminar(placa);
+    }
+
+    public boolean tieneCupos(String placa) throws Exception {
+        Bus v = repo.buscarPorPlaca(placa);
+        if (v == null) {
+            throw new Exception("No existe un Bus con la placa: " + placa);
+        }
+        return v.getCapacidad() > 0;
+    }
+}
