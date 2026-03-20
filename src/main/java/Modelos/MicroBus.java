062f3ba69fa6b0c6b3b301f9992897643436b63a
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-19 19:02:29 -0500

    feat: creación clase MicroBus (subclase)

---
+++ src/main/java/Modelos/MicroBus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+import Model.Vehiculo;
+
+/**
+ *
+ * @author HP
+ */
+public class MicroBus extends  Vehiculo{
+     public MicroBus(String placa, String ruta, boolean estado, int capacidad, double tarifa){
+       super(placa, ruta, estado, capacidad, tarifa);
+   }
+}
