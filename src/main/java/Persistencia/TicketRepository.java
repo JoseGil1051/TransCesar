1511ed82f4566db004d8bcc66d9528b1094a06de
Jose Miguel Gil <jmiguelgil@unicesar.edu.co>
2026-03-19 22:36:14 -0500

    feat: Creacion de ticketRepository con sus metodos

---
+++ src/main/java/Persistencia/TicketRepository.java
@@ -0,0 +1,71 @@
+package Persistencia;
+
+import Modelos.Ticket;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketRepository {
+    private final String archivo = "RegistroTicket.txt";
+    
+    private String toCSV(Ticket t) {
+        return t.getIdTicket() + "," +
+               t.getCedulaPasajero()  + "," +
+               t.getPlacaVehiculo() + "," +
+               t.getFechaCompra() + "," +
+               t.getOrigenRuta() + "," +
+               t.getDestinoRuta() + "," +
+               t.getValorFinal() + "," +
+               t.getTipoDescuento();
+    }
+    
+    public void crearTicket(Ticket t) throws IOException{
+        try (PrintWriter pw = new PrintWriter(new FileWriter(archivo, true))) {
+            pw.println(toCSV(t));
+        }
+    }
+    
+    public List<Ticket> listar() throws IOException {
+        List<Ticket> lista = new ArrayList<>();
+        File f = new File(archivo);
+        if (!f.exists()) return lista;
+
+        BufferedReader tk = new BufferedReader(new FileReader(archivo));
+        String linea;
+        while ((linea = tk.readLine()) != null) {
+            if (linea.trim().isEmpty()) continue;
+            String[] datos = linea.split(",");
+            int idTicket = Integer.parseInt(datos[0]);
+            int cedulaPasajero = Integer.parseInt(datos[1]);
+            String placaVehiculo = datos[2];
+            String fechaCompra = datos[3];
+            String origenRuta = datos[4];
+            String destinoRuta = datos[5];
+            double valorFinal = Double.parseDouble(datos[3]);
+            double tipoDescuento = Double.parseDouble(datos[4]);
+
+            Ticket t = new Ticket(idTicket, cedulaPasajero, placaVehiculo, fechaCompra, origenRuta, destinoRuta, valorFinal, tipoDescuento) {};
+            lista.add(t);
+        }
+        tk.close();
+        return lista;
+    }
+    
+    public void cancelar(int id) throws IOException {
+        List<Ticket> lista = listar();
+        PrintWriter tk = new PrintWriter(new FileWriter(archivo));
+        for (Ticket t : lista) {
+            if (id == t.getIdTicket()) {
+                t.setEstadoTicket(false);
+            }
+            tk.println(toCSV(t));
+        }
+        tk.close();
+    }
+    
+}
