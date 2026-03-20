a62da5c31873883c03c43045faffb9a771a9a607
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-03-19 20:59:56 -0500

    feat: clase PersonaService con llamado a persistencia

--- src/main/java/Logica/PersonaService.java
+++ src/main/java/Logica/PersonaService.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logica;
 
-/**
- *
- * @author Nacho
- */
+import Modelos.Conductor;
+import Modelos.Pasajero;
+import Modelos.Persona;
+import Modelos.TipoPasajero;
+import Persistencia.PersonaRepository;
+import java.util.List;
+import java.util.Optional;
+
 public class PersonaService {
-    
+
+    private PersonaRepository repo = new PersonaRepository();
+
+    public boolean crearConductor(String nombre, int cedula, int numeroLicencia, String categoria) {
+        return repo.crear(new Conductor(nombre, cedula, numeroLicencia, categoria));
+    }
+
+    public boolean crearPasajero(String nombre, int cedula, TipoPasajero tipo) { // ← CAMBIADO
+        return repo.crear(new Pasajero(nombre, cedula, tipo));
+    }
+
+    public Optional<Persona> buscarPorCedula(int cedula) {
+        return repo.buscarPorCedula(cedula);
+    }
+
+    public List<Persona> buscarPorNombre(String nombre) {
+        return repo.buscarPorNombre(nombre);
+    }
+
+    public void listarTodos() {
+        repo.listarTodos();
+    }
+
+    public boolean actualizarNombre(int cedula, String nuevoNombre) {
+        return repo.actualizarNombre(cedula, nuevoNombre);
+    }
+
+    public boolean actualizarConductor(int cedula, int nuevoNumLicencia, String nuevaCategoria) {
+        return repo.actualizarConductor(cedula, nuevoNumLicencia, nuevaCategoria);
+    }
+
+    public boolean actualizarPasajero(int cedula, TipoPasajero nuevoTipo) { // ← CAMBIADO
+        return repo.actualizarPasajero(cedula, nuevoTipo);
+    }
+
+    public boolean eliminar(int cedula) {
+        return repo.eliminar(cedula);
+    }
 }
