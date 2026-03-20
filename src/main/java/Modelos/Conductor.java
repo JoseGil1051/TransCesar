0ac9cb1c2294d46209fe635de125470387d072d7
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-03-19 20:38:47 -0500

    feat: Creacion de la clase hija conductor con sus categorias y atributos

---
+++ src/main/java/Modelos/Conductor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author Nacho
+ */
+public class Conductor {
+    private static final List<String> CATEGORIAS_VALIDAS = List.of("B1", "B2", "C1", "C2");
+
+    private int numerodelicencia;
+    private String categorialicencia;
+
+}
