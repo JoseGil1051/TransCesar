8d80ad5dc874798c33576e62b1bf7364d42f3dc0
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-03-19 20:36:16 -0500

    feat: clase Persona con atributos y encapsulamiento y metodo toString

--- src/main/java/Modelos/Persona.java
+++ src/main/java/Modelos/Persona.java
@@ -2,5 +2,39 @@
 package Modelos;
 
 public class Persona {
+    protected String nombre;
+    protected int cedula;
     
+    public Persona() {
+    }
+
+    public Persona(String nombre, int cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    public int getCedula() {
+        return cedula;
+    }
+
+    
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }    
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", cedula=" + cedula + '}';
+    }
 }
