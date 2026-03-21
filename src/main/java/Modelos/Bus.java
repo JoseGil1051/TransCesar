48736e8531433b7e5f26faa9764cb466a5a0bf07
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-20 23:14:17 -0500

    feat: implementacion de ruta en Bus

--- src/main/java/Modelos/Bus.java
+++ src/main/java/Modelos/Bus.java
@@ -6,7 +6,7 @@ public class Bus extends Vehiculo{
     private static final int CAPACIDAD_BUS = 45;
     private static final double TARIFA_BUS = 15000;
 
-    public Bus(String placa, String ruta, boolean estado) {
+    public Bus(String placa, Ruta ruta, boolean estado) {
         super(placa, ruta, estado, CAPACIDAD_BUS, TARIFA_BUS);
     }
 }
