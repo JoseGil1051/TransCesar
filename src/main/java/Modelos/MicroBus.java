1954195972786c0ea271cff310727709815693fe
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-20 23:15:38 -0500

    feat: implementacion de ruta en MicroBus

--- src/main/java/Modelos/MicroBus.java
+++ src/main/java/Modelos/MicroBus.java
@@ -4,7 +4,7 @@ public class MicroBus extends Vehiculo {
     private static final int CAPACIDAD_MICROBUS = 25;
     private static final double TARIFA_MICROBUS = 10000;
 
-    public MicroBus(String placa, String ruta, boolean estado) {
+    public MicroBus(String placa, Ruta ruta, boolean estado) {
         super(placa, ruta, estado, CAPACIDAD_MICROBUS, TARIFA_MICROBUS);
     }
 }
