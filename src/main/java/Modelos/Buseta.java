ed1e7842a35e747bfc8517fa1b10ffaa3c5f71da
Jose Hernandez <jdhernandezmercado@unicesar.edu.co>
2026-03-20 23:15:01 -0500

    feat: implementacion de ruta en Buseta

--- src/main/java/Modelos/Buseta.java
+++ src/main/java/Modelos/Buseta.java
@@ -5,7 +5,7 @@ public class Buseta extends Vehiculo {
     private static final int CAPACIDAD_BUSETA = 19;
     private static final double TARIFA_BUSETA = 8000;
 
-    public Buseta(String placa, String ruta, boolean estado) {
+    public Buseta(String placa, Ruta ruta, boolean estado) {
         super(placa, ruta, estado, CAPACIDAD_BUSETA, TARIFA_BUSETA);
     }
 }
